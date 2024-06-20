ce54f6bb0ee3d9e1eb7f1ca0f677c0d775a08844
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2024-06-20 16:46:40 +0700

    first commit

---
+++ assets/new-dashboard/js/maps-leaflet.js
@@ -0,0 +1,16 @@
+"use strict";
+! function () {
+    // var t = (t && (t = L.map("shapeMap").setView([51.5, -.09], 12), L.marker([51.5, -.09]).addTo(t), L.circle([51.508, -.11], {
+    //     color: "red",
+    //     fillColor: "#f03",
+    //     fillOpacity: .5,
+    //     radius: 500
+    // }).addTo(t)), document.getElementById("dragMap")),
+    //     t = (t && (t = L.map("dragMap").setView([48.817152, 2.455], 12), L.marker([48.817152, 2.455], {
+    //         draggable: "true"
+    //     }).addTo(t).bindPopup("<b>You're hereeeee!</b>").openPopup(), L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+    //         maxZoom: 18
+    //     }).addTo(t)), document.getElementById("userLocation"));
+
+}();
